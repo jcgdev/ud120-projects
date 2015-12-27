57dfae8aeaeac1e23cad7151b8770a87b3340d82
jcgdev <jcgdev@groeli.com>
2015-12-26 23:24:52 -0800

    Add code for naive bayes mini-test

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,6 +26,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = clf.score(features_train, labels_train)
+print "accuracy is:"
+print accuracy
+
+
 
 
 #########################################################
